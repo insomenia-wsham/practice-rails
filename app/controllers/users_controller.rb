fb7b30fcb761a1b42e14db7dea918c20646186cc
함우성 <wsham@insomenia.com>
2021-06-29 16:05:37 +0900

    기본정보수정 버그 수정

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApiController
   def update
-    user = current_api_user
-    user.update(user_params)
+    user = User.find(current_api_user.id)
+    user.update(name: user_params[:name]) 
     render json: serialize(user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email)
   end
 end
